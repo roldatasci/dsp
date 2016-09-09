744565dafdd1b4b5a1b098d4fafd51e2fe97f83a
Ron Olshansky-Lucero <rol-mbp@Rons-MacBook-Pro.local>
2016-09-08 22:05:13 -0700

    05a-python done

--- python/q8_parsing.py
+++ python/q8_parsing.py
@@ -4,3 +4,29 @@
 # against opponents, and had 36 goals scored against them). Write a program to read the file, 
 # then print the name of the team with the smallest difference in ‘for’ and ‘against’ goals.
 
+import os
+
+print(os.getcwd())
+os.chdir('metisgh/prework/dsp/python')  # change to directory with script and data
+print(os.getcwd())
+
+import csv
+
+f = open('football.csv', 'r')
+data_list = list(csv.reader(f))
+header = data_list[0]  # isolate header
+data = data_list[1:]  # rows of data (no header)
+print(header)  # ['Team', 'Games', 'Wins', 'Losses', 'Draws', 'Goals', 'Goals Allowed', 'Points']
+print(data[0])  # first row = ['Arsenal', '38', '26', '9', '3', '79', '36', '87']
+
+# idx 5 is goals
+# idx 6 is goals allowed
+
+def smallest_diff(data):
+    teams = [row[0] for row in data]
+    diffs = [abs(int(row[5]) - int(row[6])) for row in data]
+    team_diffs = list(zip(diffs, teams))
+    team_diffs.sort()
+    return team_diffs[0][1] # return first tuple's second element
+
+print(smallest_diff(data)) # Aston_Villa
